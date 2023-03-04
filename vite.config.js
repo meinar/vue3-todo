3a9686ece2d556e3401a16eccacd471648bd85e0
Timar <11074539+meinar@users.noreply.github.com>
2023-03-04 17:12:08 +0000

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // set base url for production to /vue3-todo/
+  base: process.env.NODE_ENV === 'production' ? '/vue3-todo/' : '/',
 })
